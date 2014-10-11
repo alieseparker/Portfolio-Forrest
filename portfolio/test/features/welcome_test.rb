6e5b968b92e99561a42c84463cca86eed90273a7
Forrest Parker <forresta.parker@gmail.com>
2014-10-11 09:57:09 -0700

    Add Article Resource, tests pass

--- portfolio/test/features/welcome_test.rb
+++ portfolio/test/features/welcome_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 feature 'As a site visitor, when visiting the root url' do
   scenario 'I wish to see a welcome page' do
     visit root_url
-    page.must_have_content "Welcome"
+    page.must_have_content 'Welcome'
   end
 end
