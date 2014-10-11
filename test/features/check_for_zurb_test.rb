404e0322713560748d14c5d670e2ce3bf6007134
Forrest Parker <forresta.parker@gmail.com>
2014-10-11 13:15:11 -0700

    fix rubocop

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
-feature "CheckForZurb" do
-  scenario "zurb should be loaded in the stylesheets" do
+feature 'CheckForZurb' do
+  scenario 'zurb should be loaded in the stylesheets' do
     visit root_path
     page.has_css? 'columns'
   end
