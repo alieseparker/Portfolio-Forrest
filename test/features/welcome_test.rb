2e351d825b1496d2fea392b282c60444d6c394b5
Forrest Parker <forresta.parker@gmail.com>
2014-10-11 13:05:06 -0700

    CH4 waiting for heroku to propogate

--- test/features/welcome_test.rb
+++ test/features/welcome_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
+require 'net/http'
 
 feature 'As a site visitor, when visiting the root url' do
   scenario 'I wish to see a welcome page' do
     visit root_url
     page.must_have_content 'Welcome'
+  end
+
+  scenario 'I wish to know that my URL works' do
+    http = Net::HTTP.new('www.forrestaparker.com', 80)
+    response = http.request_get('/')
+    p response.code
   end
 end
